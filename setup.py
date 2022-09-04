7df816805b13c3f9c6c5b44eb7b7160456e1a7ac
kelly_huang <kellylink501@gmail.com>
2022-09-04 13:37:06 +0800

    test

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+# readme.md = github readme.md, 這裡可接受markdown寫法
+# 如果沒有的話，需要自己打出介紹此專案的檔案，再讓程式知道
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TestPypi",
+    version="0.0.1",
+    author="kelly501",
+    author_email="kellylink501@gmail.com",
+    description="Test a small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kelly501/TestPypi",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    packages=['TestPypi'],
+)
